da5e623463a63f21a67cc8de6359c0552ff1bcc5
Kevin Wei <chenghaowei@gmail.com>
2019-11-11 14:15:17 -0500

    add palindromes

--- algorithms.js
+++ algorithms.js
@@ -26,3 +26,35 @@ function removeDupes(string) {
 
 let testString = 'google all that you think can think of';
 // console.log(removeDupes(testString));
+
+function isPalindrome(string) {
+  let hm = new HashMap();
+  for (let i = 0; i < string.length; i++) {
+    let key = string[i];
+    let value;
+    try {
+      value = hm.get(key);
+    } catch (error) { }
+    if (value !== undefined) {
+      hm.set(key, ++value);
+    }
+    else {
+      hm.set(key, 1);
+    }
+  }
+
+  let numOdds = 0;
+  for (let i = 0; i < string.length; i++) {
+    let key = string[i];
+    let value = hm.get(key);
+    let isOdd = value % 2 > 0;
+    if (isOdd) {
+      numOdds++;
+    }
+  }
+
+  return numOdds <= 1;
+}
+
+let testString2 = 'd0tet0d';
+// console.log(isPalindrome(testString2));
