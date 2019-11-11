633aeb00615b78da07da0cc96c519da2b961bb5d
Marlon Agno <mdj.agno@gmail.com>
2019-11-11 15:35:01 -0500

    started #6

--- algorithms.js
+++ algorithms.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const HashMap = require('./HashMap');
 
 function removeDupes(string) {
@@ -58,3 +60,80 @@ function isPalindrome(string) {
 
 let testString2 = 'd0tet0d';
 // console.log(isPalindrome(testString2));
+
+function stringToHashMap(string) {
+  let hm = new HashMap;
+  for (let i = 0; i < string.length; i++){
+    let key = string[i];
+    let value;
+
+    try{
+      value = hm.get(key);
+    }
+    catch(err){}
+    if(value !== undefined){
+      hm.set(key, ++value);
+    }
+    else{
+      hm.set(key, 1);
+    }
+  }
+  
+  return hm;
+}
+
+// function isAnagram(string1, string2){
+//   for (let i = 0; i < hm.length; i++){
+//     let key = string[i];
+//     let value;
+//     try{
+//       value = hm.get(key);
+//     }
+//     catch(err){
+//       return false;
+//     }
+//   }
+// }
+
+function anagram(words){
+  let resultshm = new HashMap;
+  while(words.length){
+    resultshm.set(words[0], []);
+    for(let i = 0; i < words.length; i++) {
+      let hm1 = stringToHashMap(words[i]);
+      let wordToCheck1 = words[i];
+      resultshm.set(wordToCheck1, []);
+  
+      for(let j = i + 1; j < words.length; j++){
+        let wordToCheck2 = words[j];
+        let hm2 = stringToHashMap(words[j]);
+        for(let k = 0; k < wordToCheck1.length; k++){
+          let key = wordToCheck1[k];
+   
+          let value = hm1.get(key);
+          let value2;
+  
+          try {
+            value2 = hm2.get(key);
+          }
+          catch(error){
+            //not an anagram
+          }
+          if(value === value2){
+            resultshm.get(wordToCheck1).push(wordToCheck2);
+            words.slice(0, j);
+          }
+        }
+      }
+      words.slice(0, i);
+    }
+  }
+
+
+  return resultshm;
+}
+
+let wordsArray = ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'];
+
+console.log(anagram(wordsArray));
+
